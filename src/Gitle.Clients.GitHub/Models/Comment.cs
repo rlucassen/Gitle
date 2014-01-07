865cd50712b50717b9e84d77a8d9c04918db6421
Robin Lucassen <robin@lucassen.me>
2014-01-07 13:33:33 +0100

    comments verbeterd

--- src/Gitle.Clients.GitHub/Models/Comment.cs
+++ src/Gitle.Clients.GitHub/Models/Comment.cs
@@ -1,19 +1,44 @@
 ﻿namespace Gitle.Clients.GitHub.Models
 {
+    using System;
+    using System.Linq;
     using System.Runtime.Serialization;
+    using System.Text.RegularExpressions;
     using Post;
 
     [DataContract]
     public class Comment
     {
+        private Regex r = new Regex(@"\(.*?\): ");
+
         [DataMember(Name = "id")]
         public virtual int Id { get; set; }
 
         [DataMember(Name = "body")]
         public virtual string Body { get; set; }
 
+        public virtual string Name
+        {
+            get
+            {
+                var match = r.Matches(Body).Cast<Match>().Select(p => p.Value).FirstOrDefault();
+                return string.IsNullOrEmpty(match) ? User.Login : match.Replace("): ", string.Empty).TrimStart('(');
+            }
+        }
+
+        public virtual string Text
+        {
+            get
+            {
+                return Body.Replace(string.Format("({0}): ", Name), string.Empty);
+            }
+        }
+
         [DataMember(Name = "user")]
         public virtual User User { get; set; }
+
+        [DataMember(Name = "created_at")]
+        public virtual DateTime CreatedAt { get; set; }
 
         public virtual CommentPost ToPost()
         {
