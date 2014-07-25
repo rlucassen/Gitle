27db5f992ddb2cfee614ccb157af516b1e9f5b6c
EdwinG <egrypma@auxilium.nl>
2014-07-25 11:39:36 +0200

    StringHelper ontbrak; overgenomen uit branch actions

---
+++ src/Gitle.Model/Helpers/StringHelper.cs
@@ -0,0 +1,32 @@
+﻿namespace Gitle.Model.Helpers
+{
+    using System.Globalization;
+    using System.Text.RegularExpressions;
+
+    public static class StringHelper
+    {
+        public static string Slugify(this string phrase)
+        {
+            var str = phrase.RemoveAccent().ToLower();
+
+            str = Regex.Replace(str, @"[^a-z0-9\s-]", ""); // invalid chars           
+            str = Regex.Replace(str, @"\s+", " ").Trim(); // convert multiple spaces into one space   
+            str = str.Substring(0, str.Length <= 45 ? str.Length : 45).Trim(); // cut and trim it   
+            str = Regex.Replace(str, @"\s", "-"); // hyphens   
+
+            return str;
+        }
+
+        public static string RemoveAccent(this string txt)
+        {
+            var bytes = System.Text.Encoding.GetEncoding("Cyrillic").GetBytes(txt);
+            return System.Text.Encoding.ASCII.GetString(bytes);
+        }
+
+        public static string ToCamelCase(this string txt)
+        {
+            var textInfo = CultureInfo.CurrentCulture.TextInfo;
+            return textInfo.ToTitleCase(txt);
+        }
+    }
+}
