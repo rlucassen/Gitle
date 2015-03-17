ed9808cc3119895d8f9510bec29a6b152754cef2
Robin Lucassen <robin@lucassen.me>
2015-03-17 10:54:47 +0100

    new files voor pdf

---
+++ src/Gitle.Model/Interfaces/Service/ITemplateParserService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Gitle.Model.Interfaces.Service
+{
+    public interface ITemplateParserService
+    {
+        string Parse(string templateName, Dictionary<string, object> propertyBag);
+    }
+}
