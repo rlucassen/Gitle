01606212a352683c47ff3099760930309b2c95df
Robin Lucassen <robin@lucassen.me>
2016-05-26 11:53:06 +0200

    kolom userrapport export weer weg

--- src/Gitle.Model/Helpers/CsvHelper.cs
+++ src/Gitle.Model/Helpers/CsvHelper.cs
@@ -49,7 +49,7 @@
         public static string ReportCsv(IList<Booking> bookings)
         {
             const string rowTemplate =
-                "\"{2}\"{0}\"{3}\"{0}\"{4}\"{0}\"{5}\"{0}\"{6}\"{0}\"{7}\"{0}\"{8}\"{0}\"{9}\"{0}{1}";
+                "\"{2}\"{0}\"{3}\"{0}\"{4}\"{0}\"{5}\"{0}\"{6}\"{0}\"{7}\"{0}\"{8}\"{0}{1}";
 
             var header = string.Format(rowTemplate, fieldseparator, lineEnd,
                                           "Minuten",
