31f04c9bdd28e3a609a0d66940af7ee085eec48d
Chelsea Vos <chelseaa@hotmail.nl>
2019-03-07 18:00:19 +0100

    Added actions in Installation controller, added property bags for edit and reworked some views

--- src/Gitle.Web/Controllers/InstallationController.cs
+++ src/Gitle.Web/Controllers/InstallationController.cs
@@ -2,6 +2,7 @@
 {
     using System.Linq;
     using Gitle.Model;
+    using Gitle.Model.Enum;
     using Gitle.Web.Helpers;
     using Model.Helpers;
     using NHibernate;
@@ -24,19 +25,64 @@
         {
             Installation installation = session.SlugOrDefault<Installation>(installationSlug);
 
-            PropertyBag.Add("installation", installation);
+            PropertyBag.Add("item", installation);
         }
 
         [Admin]
         public void Edit()
         {
+            var installationTypes = EnumHelper.ToDictionary(typeof(InstallationType));
+            var types = installationTypes.Where(t =>
+                    new[] { InstallationType.Live, InstallationType.Acceptance, InstallationType.Demo }.Contains((InstallationType)t.Key))
+                .ToList();
 
+            PropertyBag.Add("applications", session.Query<Application>().Where(x => x.IsActive).OrderBy(x => x.Name));
+            PropertyBag.Add("servers", session.Query<Server>().Where(x => x.IsActive).OrderBy(x => x.Name));
+            PropertyBag.Add("installationTypes", types);
+            PropertyBag.Add("item", new Installation());
+            RenderView("edit");
         }
-        
-        [Admin]
-        public void Delete()
-        {
 
+        [Admin]
+        public void New()
+        {
+            PropertyBag.Add("item", new Installation());
+            RenderView("edit");
+        }
+
+        [Admin]
+        public void Save(string installationSlug)
+        {
+            var item = session.SlugOrDefault<Installation>(installationSlug);
+            if (item != null)
+            {
+                BindObjectInstance(item, "item");
+            }
+            else
+            {
+                item = BindObject<Installation>("item");
+            }
+
+            using (var tx = session.BeginTransaction())
+            {
+                session.SaveOrUpdate(item);
+                tx.Commit();
+            }
+
+            RedirectToUrl("/installation");
+        }
+
+        [Admin]
+        public void Delete(string installationSlug)
+        {
+            var installation = session.SlugOrDefault<Installation>(installationSlug);
+            installation.Deactivate();
+            using (var tx = session.BeginTransaction())
+            {
+                session.SaveOrUpdate(installation);
+                tx.Commit();
+            }
+            RedirectToReferrer();
         }
     }
 }
