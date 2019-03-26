27ae02c4cae636d501dd30b6cc89d893eccc1061
Chelsea Vos <chelseaa@hotmail.nl>
2019-03-26 11:01:22 +0100

    #100 - De zinsopbouw voor bewerkingen aan tickets is niet meer geheel correct - now also in emails and the subject

--- src/Gitle.Model/ChangeState.cs
+++ src/Gitle.Model/ChangeState.cs
@@ -19,6 +19,7 @@
             get
             {
                 var state = Language.ResourceManager.GetString(IssueState.ToString()).ToLower();
+                state = (state == "open") ? "geopend" : state;
                 var openings = Issue.ChangeStates.Where(x => x.IssueState == IssueState.Open).ToList();
                 if (openings.Count() > 1 && openings.OrderByDescending(x => x.CreatedAt).Last() != this &&
                     IssueState == IssueState.Open)
@@ -52,6 +53,7 @@
         {
             get {
                 var state = Language.ResourceManager.GetString(IssueState.ToString()).ToLower();
+                state = (state == "open") ? "geopend" : state;
                 var openings = Issue.ChangeStates.Where(x => x.IssueState == IssueState.Open).ToList();
                 if (openings.Count() > 1 && openings.OrderByDescending(x => x.CreatedAt).Last() != this &&
                     IssueState == IssueState.Open)
